864a8bd0115b7cb23277109ad4aa1b5232cc4732
EkaterinaStruch <118853191+EkaterinaStruch@users.noreply.github.com>
2022-12-07 21:07:33 +0300

    Лабораторная работа 5 Исправленная

--- Лабораторная работа 5 Стручкова Е.В./task_5.py
+++ Лабораторная работа 5 Стручкова Е.В./task_5.py
@@ -1,7 +1,7 @@
-import string, random
-def get_random_password() -> str:
-    chars_ = string.ascii_uppercase + string.ascii_lowercase + string.digits
-    length_ = 8
-    password = "".join(random.sample(chars_, length_))
+from string import ascii_uppercase, ascii_lowercase, digits
+from random import sample
+def get_random_password(length_=8) -> str:
+    chars_ = ascii_uppercase + ascii_lowercase + digits
+    password = "".join(sample(chars_, length_))
     return password
 print(get_random_password())
